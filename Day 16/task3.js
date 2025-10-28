7034884abf2538476acca052a412e36793fbe37b
Vijaya-Parthiban <vijayaparthiban2512@gmail.com>
2025-10-28 13:56:41 +0530

    tasks

---
+++ Day 16/task3.js
@@ -0,0 +1,8 @@
+// Task 3: Declare 3 variables and display them in console
+let Name = "Vijaya";
+let age = 22;
+let city = "Chennai";
+
+console.log("Name:", Name);
+console.log("Age:", age);
+console.log("City:", city);
